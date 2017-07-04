f60a499b6c8bb121d4f6f9696de52ce52e1b516e
Ben Scholom <bscholom@gmail.com>
2017-07-04 15:47:38 +0300

    added matching capability so user can see matches and cant click during a false comparison

--- memory_game/memory_game.js
+++ memory_game/memory_game.js
@@ -1,4 +1,5 @@
 memoryGame = {};
+//the one namespace to rule them all
 
 memoryGame.imagesEasy = ["Memory_game_images/1.jpg", "Memory_game_images/1.jpg",  
 					"Memory_game_images/2.jpg", "Memory_game_images/2.jpg", 
@@ -7,6 +8,31 @@ memoryGame.imagesEasy = ["Memory_game_images/1.jpg", "Memory_game_images/1.jpg",
 					"Memory_game_images/5.jpg","Memory_game_images/5.jpg", 
 					"Memory_game_images/6.jpg","Memory_game_images/6.jpg"];
 //image array containing 2 of six different images
+
+memoryGame.imagesMedium = ["Memory_game_images/1.jpg", "Memory_game_images/1.jpg",  
+					"Memory_game_images/2.jpg", "Memory_game_images/2.jpg", 
+					"Memory_game_images/3.jpg", "Memory_game_images/3.jpg", 
+					"Memory_game_images/4.jpg", "Memory_game_images/4.jpg", 
+					"Memory_game_images/5.jpg","Memory_game_images/5.jpg",
+					"Memory_game_images/6.jpg","Memory_game_images/6.jpg",
+					"Memory_game_images/7.jpg","Memory_game_images/7.jpg",
+					"Memory_game_images/8.jpg","Memory_game_images/8.jpg", 
+					"Memory_game_images/9.jpg","Memory_game_images/9.jpg"];
+//image array containing 2 of nine different images
+
+memoryGame.imagesHard = ["Memory_game_images/1.jpg", "Memory_game_images/1.jpg",  
+					"Memory_game_images/2.jpg", "Memory_game_images/2.jpg", 
+					"Memory_game_images/3.jpg", "Memory_game_images/3.jpg", 
+					"Memory_game_images/4.jpg", "Memory_game_images/4.jpg", 
+					"Memory_game_images/5.jpg","Memory_game_images/5.jpg",
+					"Memory_game_images/6.jpg","Memory_game_images/6.jpg", 
+					"Memory_game_images/7.jpg","Memory_game_images/7.jpg",
+					"Memory_game_images/8.jpg","Memory_game_images/8.jpg",
+					"Memory_game_images/9.jpg","Memory_game_images/9.jpg",
+					"Memory_game_images/10.jpg","Memory_game_images/10.jpg",
+					"Memory_game_images/11.jpg","Memory_game_images/11.jpg",
+					"Memory_game_images/12.jpg","Memory_game_images/12.jpg"];
+//image array containing 2 of twelve different images
 
 memoryGame.random = function(array) {
 	var j, x, i;
@@ -46,14 +72,51 @@ memoryGame.dealCards = function() {
 
 
 memoryGame.clicked = 0;
+memoryGame.correct = 0;
+memoryGame.wrong = 0;
+memoryGame.stop = false;
 var choice1;
 var choice2;
+//seting game logic variables include card click tracker, choice variables, pause variable and score variables
 memoryGame.reveal = function(event) {
+	if(memoryGame.stop == false) {	
 	var choice = event.target;
 	var choiceId = choice.id;
 	choice.style.backgroundImage = "url("+memoryGame.imagesEasy[choiceId]+")";
 	choice.style.backgroundOrigin = "content-box";
-	choice.style.backgroundPosition = "center";
+	choice.style.backgroundRepeat = "no-repeat";
+	//event listener linked to clicking a card that changes background image to one of the animal pictures
+	if (memoryGame.clicked == 0) {
+		choice1 = choice;
+		memoryGame.clicked = 1;
+	}
+	//if no card has been clicked (is face up), this the card chosen is choice 1
+	else {
+		choice2 = choice;
+		if (choice1.style.backgroundImage == choice2.style.backgroundImage) {
+			memoryGame.correct++;
+			memoryGame.clicked = 0;
+		}
+		// if there is one face up card, this click is the second card. the images displayed are compared
+		// if the images are the same, we have a match and the cards will stay face up and the tracker goes to zero
+		// player gets a point for the match
+		else {
+			memoryGame.stop = true;
+			//pause so you cant click cards while the 2 you already selected are displayed
+			setTimeout(function() {
+			memoryGame.clicked = 0;
+			memoryGame.wrong++;
+			choice1.style.backgroundImage = "url('Memory_game_images/texture.jpg')";
+			choice2.style.backgroundImage = "url('Memory_game_images/texture.jpg')";
+			memoryGame.stop = false;
+			//restart the game
+		}, 1250);
+		// if the images are not the same, the cards will remain face up for a short time before going back to background
+		// player gets a failure point. this is not a good thing.
+		// the click tracker resets
+		}
+	}
+}
 };
 
 
